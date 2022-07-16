a8986cd96969554a0e6e6d95fea4dc7ce8993ff9
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-07-16 20:35:31 +0700

    *create window from ui toolkit

--- Assets/_Root/Editor/Dashboard.cs
+++ Assets/_Root/Editor/Dashboard.cs
@@ -1,9 +1,30 @@
 using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
 
-namespace Pancake.Editor
+
+public class Dashboard : EditorWindow
 {
-    public class Dashboard : EditorWindow
+    [MenuItem("Window/UI Toolkit/Dashboard")]
+    public static void ShowExample()
     {
-        
+        Dashboard wnd = GetWindow<Dashboard>();
+        wnd.titleContent = new GUIContent("Dashboard");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        VisualElement label = new Label("Hello World! From C#");
+        root.Add(label);
+
+        // Import UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/_Root/Editor/Resources/dashboard.uxml");
+        VisualElement labelFromUXML = visualTree.Instantiate();
+        root.Add(labelFromUXML);
     }
 }
